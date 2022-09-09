01008b7dc56d5bcabd75bf34291a8516238eae17
Roman Laputyn <laputyn.roman@gmail.com>
2022-09-09 09:27:39 -0600

    templates

--- resume/myinfo/views.py
+++ resume/myinfo/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'myinfo/index.html', {})
+    return render(request, 'myinfo/index.html', {'title': 'Resume'})
